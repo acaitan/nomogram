4629f4ec1617d3efa25371d12c497f47062d6202
acaitan <acaitan@163.com>
2025-10-29 11:37:43 +0800

    new feature

--- nomogram.py
+++ nomogram.py
@@ -20,7 +20,6 @@ def generate_df_rank(path, total_point=100):
     df = df.drop(index=["intercept", "threshold"])
     
     df = df.reset_index()
-
     
     df["sequence"] = list(range(0,df.shape[0]))
     
